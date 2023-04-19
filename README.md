b9aaabd15550ac1c94afeb0d155aca051e7f6237
faustusdotbe <simon.hengchen@gmail.com>
2023-04-19 08:36:17 +0200

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# Unprotect excel sheets
+
+1. Unzip the excel sheet in a directory within this directory
+2. `python remove_protection.py $directory`
+3. See if the newly created `new_excel.xlsx` opens correctly
---
+++ remove_protection.py
@@ -0,0 +1,24 @@
+import os, sys, re
+
+REGEX = "<sheetProtection.+?>"
+
+input_d = sys.argv[1]
+sheets = [os.path.join(input_d, "xl/worksheets", file) for file in os.listdir(os.path.join(input_d, "xl/worksheets")) if file.endswith(".xml")]
+
+
+def unprotect_sheet(sheet):
+	print(sheet)
+	with open(sheet) as f:
+		t = f.read()
+	#print(len(t))
+	t = re.sub(REGEX, "", t)
+	#print(len(t))
+	os.remove(sheet)
+	if len(t) > 0:
+		with open(sheet, "w") as f:
+			f.write(t)
+		
+for sheet in sheets:
+	unprotect_sheet(sheet)
+
+os.system(f"cd {input_d} &&  zip -r new_excel.zip . && mv new_excel.zip ../new_excel.xlsx")
